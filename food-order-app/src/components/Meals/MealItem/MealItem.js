e3169efaf4d528dbbf840c5e6a93a5a11ed5d661
Zdenko Čepan <zdenkocepan@MacStudlaZdenko-125.docsis.vodafone.cz>
2023-03-23 19:14:13 +0100

    add cart logic

--- food-order-app/src/components/Meals/MealItem/MealItem.js
+++ food-order-app/src/components/Meals/MealItem/MealItem.js
@@ -1,9 +1,23 @@
-import MealItemForm from "./MealItemForm";
+import CartContext from "../../../store/cart-context";
 
+import MealItemForm from "./MealItemForm";
 import classes from "./MealItem.module.css";
+import { useContext } from "react";
 
 export default props => {
+    const cartCtx = useContext(CartContext);
+
     const price = `$${props.price.toFixed(2)}`;
+
+    const addToCartHandler = amount => {
+        cartCtx.addItem({
+            id: props.id,
+            name: props.name,
+            amount: amount,
+            price: props.price
+        });
+    };
+
     return (
         <li className={classes.meal}>
             <div>
@@ -12,7 +26,7 @@ export default props => {
                 <div className={classes.price}>{price}</div>
             </div>
             <div>
-                <MealItemForm id={props.id}/>
+                <MealItemForm onAddToCart={addToCartHandler} id={props.id}/>
             </div>
         </li>
     );
